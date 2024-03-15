a57596ccd3dc28d91d1d1e9d2066b1ac6d2808d3
Daniel Carvalho <daniel.m.carvalho@tecnico.ulisboa.pt>
2024-03-15 22:19:08 +0000

    feat(enrollment): Added the Apply for Activity button in VolunteerActivitiesView. Closes #74

--- frontend/src/views/volunteer/VolunteerActivitiesView.vue
+++ frontend/src/views/volunteer/VolunteerActivitiesView.vue
@@ -40,6 +40,18 @@
             </template>
             <span>Report Activity</span>
           </v-tooltip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon
+                class="mr-2 action-button"
+                color="blue"
+                v-on="on"
+                data-cy="applyButton"
+                >login</v-icon
+              >
+            </template>
+            <span>Apply for Activity</span>
+          </v-tooltip>
         </template>
       </v-data-table>
     </v-card>
