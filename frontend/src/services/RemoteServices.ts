56967c13507a6a9f205f51d1f1c96d3085c17a65
Joao Martins <joao.l.martins@tecnico.ulisboa.pt>
2024-03-18 17:23:25 +0000

    feat(Assessment): Implemented the remote services for communication with webservices
    Related to #87

--- frontend/src/services/RemoteServices.ts
+++ frontend/src/services/RemoteServices.ts
@@ -13,6 +13,7 @@ import AuthPasswordDto from '@/models/user/AuthPasswordDto';
 import Theme from '@/models/theme/Theme';
 import Enrollment from '@/models/enrollment/Enrollment';
 import Assessment from '@/models/assessment/Assessment';
+import Participation from '@/models/participation/Participation';
 
 const httpClient = axios.create();
 httpClient.defaults.timeout = 100000;
@@ -507,7 +508,46 @@ export default class RemoteServices {
       });
   }
 
+  // Participation controller
+
+  static async getVolunteerParticipations(): Promise<Participation[]> {
+    return httpClient
+      .get('/participations/volunteer')
+      .then((response) => {
+        return response.data.map((participation: any) => {
+          return new Participation(participation);
+        });
+      })
+      .catch(async (error) => {
+        throw Error(await this.errorMessage(error));
+      });
+  }
+
   // Assessment Controller
+
+  static async createAssessment(institutionId: number, assessment: Assessment) {
+    return httpClient
+      .post(`/institutions/${institutionId}/assessments`, assessment)
+      .then((response) => {
+        return new Assessment(response.data);
+      })
+      .catch(async (error) => {
+        throw Error(await this.errorMessage(error));
+      });
+  }
+
+  static async getVolunteerAssessments(): Promise<Assessment[]> {
+    return httpClient
+      .get('/assessments/volunteer')
+      .then((response) => {
+        return response.data.map((assessment: any) => {
+          return new Assessment(assessment);
+        });
+      })
+      .catch(async (error) => {
+        throw Error(await this.errorMessage(error));
+      });
+  }
 
   static async getInstitutionAssessments(
     institutionId: number | null,
