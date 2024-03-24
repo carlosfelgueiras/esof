b20d09fc4c64a3730b238dc6683167443d06ca94
Pedro Frigolet <pedro.frigolet@tecnico.ulisboa.pt>
2024-03-24 00:09:48 +0000

    fix(participation): Fixed formatting of the participation dialog
    Related to #103

--- frontend/src/views/member/ParticipationDialog.vue
+++ frontend/src/views/member/ParticipationDialog.vue
@@ -1,60 +1,61 @@
 <template>
-    <v-dialog v-model="dialog" persistent width="600">
-      <v-card>
-        <v-card-title>
-          <span class="headline"> Select Participant </span>
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" lazy-validation>
-            <v-row>
-              <v-col cols="12">
-                <v-text-field
-                  label="Rating"
-                  v-model="participation.rating"
-                  data-cy="ratingInput"
-                ></v-text-field>
-              </v-col>
-            </v-row>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="blue-darken-1"
-            variant="text"
-            @click="$emit('close-participation-dialog')"
-          >
-            Close
-          </v-btn>
-          <v-btn
-            color="blue-darken-1"
-            variant="text"
-            data-cy="saveParticipation"
-            @click="onSaveParticipation"
-          >
-            Make Participant
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </template>
-  
-  <script lang="ts">
-  import { Vue, Component, Prop, Model } from 'vue-property-decorator';
-  import Participation from '@/models/participation/Participation';
-  
-  @Component({})
-  export default class ParticipationDialog extends Vue {
-    @Model('dialog', { type: Boolean }) dialog!: boolean;
-    @Prop({ type: Number, required: true }) readonly activityId!: number;
-  
-    participation: Participation = new Participation();
-  
-    async onSaveParticipation() {
-      //TODO: implement
-      this.$emit('save-participation');
-    }
+  <v-dialog v-model="dialog" persistent width="600">
+    <v-card>
+      <v-card-title>
+        <span class="headline"> Select Participant </span>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                label="Rating"
+                v-model="participation.rating"
+                data-cy="ratingInput"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          @click="$emit('close-participation-dialog')"
+        >
+          Close
+        </v-btn>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          data-cy="saveParticipation"
+          @click="onSaveParticipation"
+        >
+          Make Participant
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Model } from 'vue-property-decorator';
+import Participation from '@/models/participation/Participation';
+import RemoteServices from '@/services/RemoteServices';
+
+@Component({})
+export default class ParticipationDialog extends Vue {
+  @Model('dialog', { type: Boolean }) dialog!: boolean;
+  @Prop({ type: Number, required: true }) readonly activityId!: number;
+
+  participation: Participation = new Participation();
+
+  async onSaveParticipation() {
+    //TODO: implement
+    this.$emit('save-participation');
   }
-  </script>
-  
-  <style scoped lang="scss"></style>
+}
+</script>
+
+<style scoped lang="scss"></style>
